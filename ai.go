de6051508ca62434e1529d1ce0f89e015b757c5d
Stig Lindqvist <stojg.lindqvist@gmail.com>
2015-07-26 16:48:02 +1200

    Fixes to the AI for vector math

--- ai.go
+++ ai.go
@@ -33,37 +33,35 @@ func (d *AIDrunkard) Update(s *Sprite, w *World, t time.Duration) {
 }
 
 func (d *AIDrunkard) Flee(w *World, s *Sprite) {
-	x, y := s.Velocity()
-	if math.Abs(x+y) == 0 {
-		if w.rand.Float32() < 0.5 {
-			x = 10
-		} else {
-			y = 10
-		}
+	if d.state != "flee" {
+		rot := w.rand.Float64() * math.Pi * 2
+		s.SetRotation(rot)
+		vel := RadiansVec2(rot).Multiply(50)
+		s.SetVelocity(vel.X, vel.Y)
+		d.state = "flee"
 	}
-	s.SetVelocity(x*10, y*10)
 }
 
-func (d *AIDrunkard) Stop(w *World, s *Sprite) {
+func (d *AIDrunkard) Idle(w *World, s *Sprite) {
+	d.state = "idle"
 	s.SetVelocity(0, 0)
 }
 
 func (d *AIDrunkard) Stagger(w *World, s *Sprite) {
+	d.state = "stagger"
 	if w.rand.Float32() < 0.99 {
 		return
 	}
 	rand := w.rand.Float32()
 	if rand > 0.75 {
 		s.Rotation = (3.14 / 2)
-		s.SetVelocity(10, 0)
 	} else if rand > 0.50 {
 		s.Rotation = (3 * 3.14 / 2)
-		s.SetVelocity(-10, 0)
 	} else if rand > 0.25 {
 		s.Rotation = 3.14
-		s.SetVelocity(0, 10)
 	} else {
 		s.Rotation = 0
-		s.SetVelocity(0, -10)
 	}
+	vel := RadiansVec2(s.Rotation).Multiply(10)
+	s.SetVelocity(vel.X, vel.Y)
 }
