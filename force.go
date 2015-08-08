4282c551fab08f4a141f559cba194ede1a215c26
Stig Lindqvist <stojg.lindqvist@gmail.com>
2015-08-09 10:35:55 +1200

    adding a very simple spring force generator

--- force.go
+++ force.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	. "github.com/stojg/pants/vector"
+	"math"
 )
 
 type ForceGenerator interface {
 	UpdateForce(*Physics, float64)
 }
-
 
 type StaticForce struct {
 	force *Vec2
@@ -17,12 +17,36 @@ func (pg *StaticForce) UpdateForce(p *Physics, duration float64) {
 	if p.invMass == 0 {
 		return
 	}
-	p.AddForce(pg.force.Multiply(1/p.invMass))
+	p.AddForce(pg.force.Multiply(1 / p.invMass))
 }
 
 type ParticleForceRegistration struct {
 	particle *Physics
-	fg ForceGenerator
+	fg       ForceGenerator
+}
+
+type SpringForce struct {
+	other           *Physics
+	springConstance float64
+	restLength      float64
+}
+
+func (sf *SpringForce) UpdateForce(p *Physics, duration float64) {
+	// calculate the vector of the spring
+	force := &Vec2{}
+	p.getPosition(force)
+	force.Sub(sf.other.Position)
+
+	// calculate the magnitude of the force
+	magnitude := force.Length()
+
+	magnitude = math.Abs(magnitude - sf.restLength)
+	magnitude *= sf.springConstance
+
+	// calculate the final force and apply it
+	force.Normalize()
+	force.Scale(-magnitude)
+	p.AddForce(force)
 }
 
 type PhysicsForceRegistry struct {
@@ -32,7 +56,7 @@ type PhysicsForceRegistry struct {
 func (pfr *PhysicsForceRegistry) Add(p *Physics, fg ForceGenerator) {
 	pfr.registrations = append(pfr.registrations, &ParticleForceRegistration{
 		particle: p,
-		fg: fg,
+		fg:       fg,
 	})
 }
 
@@ -45,7 +69,7 @@ func (pfr *PhysicsForceRegistry) Clear() {
 }
 
 func (pfr *PhysicsForceRegistry) Update(duration float64) {
-	for _, i:= range pfr.registrations {
+	for _, i := range pfr.registrations {
 		i.fg.UpdateForce(i.particle, duration)
 	}
 }
