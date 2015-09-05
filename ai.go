3268d0bc3dc602cc0ef435ee86e3d05f0b1a8dd4
Stig Lindqvist <stojg.lindqvist@gmail.com>
2015-09-05 18:24:35 +1200

    make the state machine a little bit more usable

--- ai.go
+++ ai.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	. "github.com/stojg/pants/ai"
-	//	. "github.com/stojg/pants/physics"
-	//	"time"
+	"github.com/stojg/pants/physics"
+	"github.com/stojg/pants/vector"
 )
 
 type AI interface {
@@ -16,100 +16,73 @@ type BasicAI struct {
 }
 
 /// Conditions
-type Timer struct{}
+type TrueCondition struct{}
 
-func (c *Timer) Test() bool {
+func (c *TrueCondition) Test() bool {
+	return true
+}
+
+type FalseCondition struct {
+}
+
+func (c *FalseCondition) Test() bool {
 	return false
 }
 
-type CloseClick struct{}
-
-func (c *CloseClick) Test() bool {
-	return false
-}
-
-type IsSafe struct{}
-
-func (c *IsSafe) Test() bool {
-	return false
-}
-
-type IsCloseTo struct{}
-
-func (c *IsCloseTo) Test() bool {
-	return false
-}
-
-// Actions
-
-type MyAction struct {
+type Hunt struct {
 	Action
+	steering Steering
 }
 
-func (a *MyAction) Act() {
-	// do something here
+func (a *Hunt) Act(id uint64) {
+	o := a.steering.Get()
+	world.Physic(id).AddForce(o.Linear.Scale(world.Physic(id).Mass()))
+	world.Physic(id).SetRotations(o.Angular)
 }
 
 // Concrete state implementation
-
-type SomeState struct {
-	transition *Transition
+type IdleState struct {
+	State
 }
 
-func (m *SomeState) Actions() Actioner {
-	return &MyAction{}
+type HuntingState struct {
+	State
+	id       uint64
+	position *vector.Vec2
+	steering Steering
 }
 
-func (m *SomeState) ExitActions() Actioner {
+func (m *HuntingState) Actions() Actioner {
+	return &Hunt{
+		steering: m.steering,
+	}
+}
+
+func (m *HuntingState) EntryActions() Actioner {
+	m.position = &vector.Vec2{world.RandF64(800), world.RandF64(600)}
+	target := physics.NewPhysics(m.position.X, m.position.Y, 0, 0)
+	m.steering = NewArrive(world.Physic(m.id), target, 10, 500)
 	return nil
-}
-
-func (m *SomeState) EntryActions() Actioner {
-	return nil
-}
-
-func (m *SomeState) Transition() *Transition {
-	return m.transition
 }
 
 // AI implementation
+func NewBasicAI(id uint64) *BasicAI {
 
-func NewBasicAI() *BasicAI {
+	idleState := &IdleState{}
+	hunting := &HuntingState{id: id}
 
-	idleState := &SomeState{}
-	seekState := &SomeState{}
-	fleeState := &SomeState{}
-
-	seekTransition := &Transition{
-		condition:   &Timer{},
-		targetState: seekState,
-		action:      nil,
-		next:        nil, // next transition
-	}
-
-	fleeTransition := &Transition{
-		condition:   &CloseClick{},
-		targetState: fleeState,
-		action:      nil,
-		next:        nil,
-	}
-
-	stopFleeingTransition := &Transition{
-		condition:   &IsSafe{},
-		targetState: seekState,
-		action:      nil,
+	huntingTransition := &Transition{
+		condition:   &TrueCondition{},
+		targetState: hunting,
 	}
 
 	idleTransition := &Transition{
-		condition:   &IsCloseTo{},
+		condition: &FalseCondition{},
 		targetState: idleState,
-		action:      nil,
 	}
 
-	seekTransition.next = fleeTransition
-	idleState.transition = seekTransition
-	seekState.transition = idleTransition
-	fleeState.transition = stopFleeingTransition
+	idleState.transition = huntingTransition
+	hunting.transition = idleTransition
 
 	return &BasicAI{
 		state: NewStateMachine(idleState),
@@ -118,64 +91,12 @@ func NewBasicAI() *BasicAI {
 
 func (d *BasicAI) Update(id uint64, w *World, t float64) {
 	//	d.handleInputs(s)
-	action := d.state.Update()
-	if action != nil {
-		for a := action.Front(); a != nil; a = action.Next() {
-			a.Act()
+	for a := d.state.Update(); a != nil; a = a.Next() {
+		if a != nil {
+			a.Act(id)
 		}
 	}
 }
 
 func (d *BasicAI) handleInputs(s *Entity) {
-	for _, input := range s.inputs {
-		switch input.Type {
-		case "mouseOver":
-		//			d.state.SetState(STATE_FLEE)
-		//		case "mouseOut":
-		//			d.state.SetState(STATE_IDLE)
-		case "clickUp":
-			//			d.state.SetState(STATE_IDLE)
-		}
-	}
-	s.inputs = nil
-}
-
-func (d *BasicAI) flee(w *World, id uint64, duration float64) *SteeringOutput {
-	return nil
-	//	if d.state.Past(time.Second) {
-	//		d.steering = nil
-	//		d.state.SetState(STATE_IDLE)
-	//		return nil
-	//	}
-	//
-	//	if _, ok := d.steering.(*Flee); !ok {
-	//		d.steering = NewFlee(w.Physic(id), NewPhysics(w.RandF64(1000), w.RandF64(1000), 0, 0))
-	//	}
-	//	return d.steering.Get(duration)
-}
-
-func (d *BasicAI) idle(w *World, id uint64, duration float64) *SteeringOutput {
-	return nil
-	//	if !d.state.Past(3*time.Second) {
-	//		return nil
-	//	}
-	//	d.steering = nil
-	//	d.state.SetState(STATE_HUNT)
-	//	return nil
-}
-
-func (d *BasicAI) hunt(w *World, id uint64, duration float64) *SteeringOutput {
-	return nil
-	//	if _, ok := d.steering.(*Arrive); !ok {
-	//		d.steering = NewArrive(w.Physic(id), NewPhysics(w.RandF64(800), w.RandF64(600), 0, 0), 10, 500)
-	//	}
-	//
-	//	direction := d.steering.Target().Position.Clone().Sub(w.Physic(id).Position)
-	//	length := direction.Length()
-	//	if length <= 10 {
-	//		d.steering = nil
-	//		d.state.SetState(STATE_IDLE)
-	//		return nil
-	//	}
-	//	return d.steering.Get(duration)
 }
--- state.go
+++ state.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"time"
+	"log"
 )
 
 type Condition interface {
@@ -13,7 +14,7 @@ type Actioner interface {
 	Back() Actioner
 	Next() Actioner
 	SetNext(Actioner)
-	Act()
+	Act(id uint64)
 	Interruptable() bool
 	CanDoBoth(other Actioner) bool
 	IsComplete() bool
@@ -48,7 +49,7 @@ func (a *Action) Back() Actioner {
 	return thisAction
 }
 
-func (a *Action) Act() {
+func (a *Action) Act(id uint64) {
 
 }
 
@@ -91,7 +92,6 @@ func (t *Transition) getTargetState() StateMachineState {
 	return t.targetState
 }
 
-// StateMachineState
 type StateMachineState interface {
 	// Transition gets the first... transition...
 	Transition() *Transition
@@ -113,6 +113,43 @@ type StateMachineState interface {
 	// instances, and the caller of this methodshould be responsible for the
 	// deletion.
 	ExitActions() Actioner
+}
+
+// StateMachineState
+type State struct {
+	transition *Transition
+}
+
+// Transition gets the first... transition...
+func (s *State) Transition() *Transition {
+	return s.transition
+}
+
+// Action returns the first in a sequence of actions that should be
+// performed while the character is in this state.
+// Note that this method should return one or more newly created action
+// instances, and the caller of this method should be responsible for the
+// deletion.
+func (s *State) Actions() Actioner {
+	return nil
+}
+
+// EntryActions returns the sequence of actions to perform when arriving in
+// this state.
+// Note that this method should return one or more newly created action
+// instances, and the caller of this method should be responsible for the
+// deletion.
+func (s *State) EntryActions() Actioner {
+	return nil
+}
+
+// ExitActions returns the sequence of actions to perform when leaving
+// this state.
+// Note that this method should return one or more newly created action
+// instances, and the caller of this methodshould be responsible for the
+// deletion.
+func (s *State) ExitActions() Actioner {
+	return nil
 }
 
 func NewStateMachine(initial StateMachineState) *StateMachine {
@@ -140,6 +177,7 @@ func (sm *StateMachine) Update() Actioner {
 			// we have nothing to do
 			return nil
 		} else {
+			log.Printf("initial state %T", sm.initialState)
 			// Transition to the first state
 			sm.currentState = sm.initialState
 			// return initial state's actions
@@ -147,16 +185,16 @@ func (sm *StateMachine) Update() Actioner {
 		}
 	}
 
-	// We have a current state to work with
-
 	// Start off with no transition
 	var transition *Transition
 
 	// Check through each transition in the current state.
 	testTransition := sm.currentState.Transition()
+//	log.Printf("transition %V", testTransition)
 	for t := testTransition.Front(); t != nil; t = t.Next() {
 		// check if this transition should trigger
 		if testTransition.isTriggered() {
+			log.Printf("transition %T triggered", testTransition)
 			transition = testTransition
 			break
 		}
@@ -167,21 +205,45 @@ func (sm *StateMachine) Update() Actioner {
 		return sm.currentState.Actions()
 	}
 
+	log.Printf("transitioning from %T to %T", sm.currentState, transition.targetState)
+
 	// we are going through a transition so create a list of actions from the
 	// - current states exit action(s)
 	// - the transitions action(s)
 	// - the next states action(s)
-
+	actions := make([]Actioner,0)
 	// get all the exit actions from the current state
-	actions := sm.currentState.ExitActions()
-	last := actions.Back()
+	exitActions := sm.currentState.ExitActions()
+	if exitActions != nil {
+		actions = append(actions, exitActions)
+	}
+
 	// get the actions from the transition
 	transitionActions := transition.Actions()
-	last.SetNext(transitionActions)
+	if transitionActions != nil {
+		actions = append(actions, transitionActions)
+	}
+
+
 	// add the actions from the next state
 	nextState := transition.getTargetState()
-	transitionActions.Back().SetNext(nextState.EntryActions())
-	// Update the change of state
+	entryActions := nextState.EntryActions()
+	if entryActions != nil {
+		actions = append(actions, entryActions)
+	}
+
 	sm.currentState = nextState
-	return actions
+
+	if len(actions) == 0 {
+		return nil
+	}
+
+	first, actions := actions[0], actions[1:]
+
+	for _, action := range actions {
+		first.Back().SetNext(action)
+	}
+
+	// Update the change of state
+	return first
 }
